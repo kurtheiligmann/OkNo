ba6c2000b06032cf71363b5be3830b3cb6694fba
Kurt Heiligmann <kurtheiligmann@gmail.com>
2015-02-13 22:04:12 -0800

    updated DataManager: - removed singleton methods in favor of instance methods - added default init method

--- app/src/main/java/com/kurtheiligmann/okno/data/DataManager.java
+++ app/src/main/java/com/kurtheiligmann/okno/data/DataManager.java
@@ -3,22 +3,62 @@ package com.kurtheiligmann.okno.data;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.kurtheiligmann.okno.R;
+
+import java.util.List;
+
 /**
  * Created by kurtheiligmann on 2/9/15.
  */
 public class DataManager {
     public static final String PREFS_NAME = "com.kurtheiligmann.okno.data.prefs";
-    private static final String ENABLED_KEY = "com.kurtheiligmann.okno.data.enabledKey";
 
-    public static void saveEnabled(boolean enabled, Context context) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean(ENABLED_KEY, enabled);
-        editor.commit();
+    private static final String ENABLED_KEY = "com.kurtheiligmann.okno.data.enabledKey";
+    private static final String DEFAULT_POSITIVE_SOUND = "android.resource://com.kurtheiligmann.okno/raw/two_tone";
+    private static final String DEFAULT_NEGATIVE_SOUND = "android.resource://com.kurtheiligmann.okno/raw/no_tone";
+
+    private Context context;
+
+    public DataManager(Context context) {
+        setContext(context);
+        initDefaultSoundsIfNecessary(getContext());
     }
 
-    public static boolean getEnabled(Context context) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+    public void saveEnabled(boolean enabled) {
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(ENABLED_KEY, enabled);
+        editor.apply();
+    }
+
+    public boolean getEnabled() {
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         return sharedPreferences.getBoolean(ENABLED_KEY, true);
+    }
+
+    private void initDefaultSoundsIfNecessary(Context context) {
+        List<Message> existingMessages = Message.listAll(Message.class);
+
+        if (existingMessages == null || existingMessages.size() == 0) {
+            String[] defaultPositiveValues = context.getResources().getStringArray(R.array.default_positive_values);
+            for (String defaultPositiveValue : defaultPositiveValues) {
+                Message message = new Message(defaultPositiveValue, DEFAULT_POSITIVE_SOUND, false);
+                message.save();
+            }
+
+            String[] defaultNegativeValues = context.getResources().getStringArray(R.array.default_negative_values);
+            for (String defaultNegativeValue : defaultNegativeValues) {
+                Message message = new Message(defaultNegativeValue, DEFAULT_NEGATIVE_SOUND, false);
+                message.save();
+            }
+        }
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
     }
 }
