b6ce044c80391ffebe952590c5eca3f0d95869ef
Kurt Heiligmann <kurtheiligmann@gmail.com>
2015-02-16 21:11:17 -0800

    added getters and setters to Message

--- app/src/main/java/com/kurtheiligmann/okno/data/Message.java
+++ app/src/main/java/com/kurtheiligmann/okno/data/Message.java
@@ -6,9 +6,9 @@ import com.orm.SugarRecord;
  * Created by kurtheiligmann on 2/11/15.
  */
 public class Message extends SugarRecord<Message> {
-    String body;
-    String audioFilePath;
-    boolean caseSensitive;
+    private String body;
+    private String audioFilePath;
+    private boolean caseSensitive;
 
     public Message() {
     }
@@ -16,6 +16,30 @@ public class Message extends SugarRecord<Message> {
     public Message(String body, String audioFilePath, boolean caseSensitive) {
         this.body = body;
         this.audioFilePath = audioFilePath;
+        this.caseSensitive = caseSensitive;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAudioFilePath() {
+        return audioFilePath;
+    }
+
+    public void setAudioFilePath(String audioFilePath) {
+        this.audioFilePath = audioFilePath;
+    }
+
+    public boolean isCaseSensitive() {
+        return caseSensitive;
+    }
+
+    public void setCaseSensitive(boolean caseSensitive) {
         this.caseSensitive = caseSensitive;
     }
 }
