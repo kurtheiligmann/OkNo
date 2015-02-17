910b619b52523f1ac5f088f9cc86b13ba08a2abd
Kurt Heiligmann <kurtheiligmann@gmail.com>
2015-02-16 23:12:13 -0800

    added message list item layout

--- app/src/main/java/com/kurtheiligmann/okno/controller/settings/fragment/SettingsFragment.java
+++ app/src/main/java/com/kurtheiligmann/okno/controller/settings/fragment/SettingsFragment.java
@@ -7,15 +7,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ListView;
 
 import com.kurtheiligmann.okno.R;
+import com.kurtheiligmann.okno.controller.settings.adapter.MessageListAdapter;
 import com.kurtheiligmann.okno.data.DataManager;
 import com.kurtheiligmann.okno.data.Message;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class SettingsFragment extends Fragment {
+
+    private MessageListAdapter messageListAdapter;
 
     public SettingsFragment() {
     }
@@ -23,27 +26,33 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
 
         final DataManager dataManager = new DataManager(getActivity());
-        CheckBox enabledCheckBox = (CheckBox)rootView.findViewById(R.id.enabled_checkbox);
+
+        CheckBox enabledCheckBox = (CheckBox) rootView.findViewById(R.id.enabled_checkbox);
         enabledCheckBox.setChecked(dataManager.getEnabled());
         enabledCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 dataManager.saveEnabled(isChecked);
                 if (isChecked) {
-                    populateMessageList(dataManager.getAllMessages());
+                    populateMessageList(dataManager.getAllMessages(), (ListView) rootView.findViewById(R.id.messages_list));
                 } else {
-                    populateMessageList(new ArrayList<Message>());
+//                    populateMessageList(new ArrayList<Message>(), (ListView) rootView.findViewById(R.id.messages_list));
                 }
             }
         });
 
+        if (dataManager.getEnabled()) {
+            populateMessageList(dataManager.getAllMessages(), (ListView) rootView.findViewById(R.id.messages_list));
+        }
+
         return rootView;
     }
 
-    private void populateMessageList(List<Message> messages) {
-
+    private void populateMessageList(List<Message> messages, ListView messageList) {
+        messageListAdapter = new MessageListAdapter(getActivity(), R.layout.message_list_item, messages.toArray(new Message[messages.size()]));
+        messageList.setAdapter(messageListAdapter);
     }
 }
