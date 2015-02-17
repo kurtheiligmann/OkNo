ff54feb57f8e99517cf2bfeacd3ad6fe8b1ed1ec
Kurt Heiligmann <kurtheiligmann@gmail.com>
2015-02-16 22:08:44 -0800

    added a checkbox and change listener

--- app/src/main/java/com/kurtheiligmann/okno/controller/settings/fragment/SettingsFragment.java
+++ app/src/main/java/com/kurtheiligmann/okno/controller/settings/fragment/SettingsFragment.java
@@ -5,15 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.kurtheiligmann.okno.R;
+import com.kurtheiligmann.okno.data.DataManager;
 import com.kurtheiligmann.okno.data.Message;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SettingsFragment extends Fragment {
-    private boolean enabled;
-    private List<Message> messages;
 
     public SettingsFragment() {
     }
@@ -22,6 +24,26 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        final DataManager dataManager = new DataManager(getActivity());
+        CheckBox enabledCheckBox = (CheckBox)rootView.findViewById(R.id.enabled_checkbox);
+        enabledCheckBox.setChecked(dataManager.getEnabled());
+        enabledCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                dataManager.saveEnabled(isChecked);
+                if (isChecked) {
+                    populateMessageList(dataManager.getAllMessages());
+                } else {
+                    populateMessageList(new ArrayList<Message>());
+                }
+            }
+        });
+
         return rootView;
+    }
+
+    private void populateMessageList(List<Message> messages) {
+
     }
 }
